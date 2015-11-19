b31f7e860f9e4acaf4def2b734f87501f7589aaa
kmorse2015 <kam672@macterm04.falabs.rutgers.edu>
2015-11-18 19:24:39 -0500

    class

---
+++ Week12-PracticeCode/lightbox/files/js/utilities.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$('project').magnificPopup({
+		type:'image'
+	});
+
+	$('.gallery').magnificPopup({
+		type:'image',
+		gallery: {
+			enabled: true
+		}
+	});
+
+});
