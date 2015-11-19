24bc11b7d38efb25bb1373ac8607a8162473abe9
kmorse2015 <kam672@macterm04.falabs.rutgers.edu>
2015-11-18 19:37:05 -0500

    class

---
+++ Assignment 2-Portfolio Website/files/JS/main.js
@@ -0,0 +1,14 @@
+$ (document). ready (function () {
+
+$('.flexslider').flexslider({
+	animation:'slide'
+})
+
+$('.flexslider2').flexslider({
+	animation:'slide', 
+	controlNav: 'thumbnails'
+})
+
+
+
+});
